2a15f611cebb2a0577e987c156e2360e8b966e21
cedec1985 <cedricdecraim@msn.com>
2022-09-01 15:38:50 +0200

    insufficient ressources

--- myexception.java
+++ myexception.java
@@ -5,27 +5,27 @@ public class myexception {
     private int number;
     public  myexception (int number){
         this.number=number;
-}
+    }
     public void deposit(double amount){
         balance += amount;
     }
-    public void withdraw(double amount) throws InsufficientFundsException{
+    public void withdraw(double amount) throws InsufficientRessourcesException{
         if (amount<= balance){
             balance -= amount;
         }
         else{
             double needs= amount - balance;
-            throw new InsufficientFundsException(needs);
+            throw new InsufficientRessourcesException(needs);
         }
     }
     public double getBalance(){
         return balance;
     }
 
-    public inr getNumber(){
+    public int getNumber(){
         return number;
     }
-
+public class bank{
     public static void main(String []args){
         myexception c = new myexception(101);
         System.out.println("depositing $500.....");
@@ -35,10 +35,9 @@ public class myexception {
             c.withdraw(100.00);
             System.out.println("\nwithdrawing $600....");
             c.withdraw(600.00);
-        }catch (InsufficientFundsException e){
+        }catch (InsufficientRessourcesException e){
             System.out.println("sorry but you are short $ "+ e.getAmount());
             e.printStackTrace();
         }
     }   
-
-}
+}}
