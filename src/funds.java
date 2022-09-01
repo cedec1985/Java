2d7d06e0e1caaceb1e03fdd68406202d665ed7ab
cedec1985 <cedricdecraim@msn.com>
2022-09-01 15:46:09 +0200

    compilattion

---
+++ src/funds.java
@@ -0,0 +1,10 @@
+public class InsufficientRessourcesException extends Exception {
+    private double amount;
+    public InsufficientRessourcesException(double amount){
+        this.amount=amount;
+
+    }
+    public double getAmount(){
+        return amount;
+    }
+}
