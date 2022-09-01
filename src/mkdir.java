60590ecd3b180b0b2c74bca14f60459090d0eb3a
cedec1985 <cedricdecraim@msn.com>
2022-09-01 14:38:23 +0200

    ok

--- src/mkdir.java
+++ src/mkdir.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class mkdir {
     public static void main (String args []){
-        String dirname = "C:/Users/édric/OneDrive/Documents/javap1/bin/creation";
+        String dirname = "C:/Users/édric/OneDrive/Documents/javap1/creation";
         File d = new File(dirname);
         d.mkdirs();
     }
