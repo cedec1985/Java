7682a1bf4947c1cd505c98ca73362b261544e9c7
cedec1985 <cedricdecraim@msn.com>
2022-09-01 14:09:32 +0200

    copie

--- src/copy.java
+++ src/copy.java
@@ -1,9 +1,8 @@
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class copyFile{
+public class copy{
     public static void main(String args[]) throws IOException{
         FileReader in =null;
         FileWriter out = null;
