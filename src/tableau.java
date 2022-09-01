0416180f1d7e8f60c741f53a2ce0891a69c46da8
cedec1985 <cedricdecraim@msn.com>
2022-09-01 17:38:21 +0200

    nouveau fichier

---
+++ src/tableau.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class tableau {
+    int [] tab;
+    tableau(int n){
+        Random rand = new Random();
+        tab = new int [n];
+        int i =0;
+        do
+        tab [i++] = rand.nextInt();
+        while (i<n);}
+        boolean rechercher (int x){
+            int i =0;
+            do
+            if (tab[i++]==x)
+            return true;
+            while(i<tab.length);
+            return false;
+        }
+    }
+
