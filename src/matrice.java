b4c25060070fa0f98e9cf6815eb850bf140dcef2
cedec1985 <cedricdecraim@msn.com>
2022-09-02 19:32:52 +0200

    matrice

---
+++ src/matrice.java
@@ -0,0 +1,8 @@
+public class matrice {
+    double [][] matrices = new double [1][2];
+    matrices [0] = new double [5];
+for (int i =0;i<1;i++)
+    for (int j=0;j<2;j++)
+matrices [i][j] = v;
+}
+
