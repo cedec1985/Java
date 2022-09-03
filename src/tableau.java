7767fc835439cfe40ec3052fbee089eb925bba88
cedec1985 <cedricdecraim@msn.com>
2022-09-03 18:14:29 +0200

    tableau

--- src/tableau.java
+++ src/tableau.java
@@ -9,13 +9,14 @@ public class tableau {
         do
         tab [i++] = rand.nextInt();
         while (i<n);}
+        public static void main (String [] args){}
+        
         boolean rechercher (int x){
             int i =0;
             do
             if (tab[i++]==x)
             return true;
             while(i<tab.length);
-            return false;
-        }
-    }
-
+            return false;}}
+            
+        
