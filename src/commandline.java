98e7757169c4a2b93f8af4c2648d5d2a0e831ca6
cedec1985 <cedricdecraim@msn.com>
2022-09-03 18:19:23 +0200

    les classes

--- src/commandline.java
+++
@@ -1,8 +0,0 @@
-public class commandline {
-    public static void main(String args[]){
-        int i =0;
-        for (i=0;i<args.length;i++){
-            System.out.println("args["+i+"]:"+args[i]);
-        } 
-    } }
-
