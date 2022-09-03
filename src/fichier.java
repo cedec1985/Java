4e4633e4af852370cb002326b59d2290cee6d90e
cedec1985 <cedricdecraim@msn.com>
2022-09-03 14:00:08 +0200

    tout construire

--- src/fichier.java
+++ src/fichier.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
-class fichier {
+public class fichier {
    private String nomFichier;
    fichier(String nom,int n) throws IOException{
     nomFichier = nom;
@@ -31,7 +31,58 @@ class fichier {
     }
     return nbelmnt ==0 ? 0:moyenne/(double) nbelmnt;
 }
+
+private void recopier (DataInputStream i, DataOutputStream o)
+{
+try{
+    while(true) o.writeInt(i.readInt());
 }
+catch (EOFException e) {
+    i.close();o.close();
+}
+
+public void fusionner (String f1, String f2)
+throws IOException, EOFException{
+    DataInputStream f = new DataInputStream(new FileInputStream(f1));
+    DataInputStream g = new DataInputStream(new FileInputStream(f2));
+    DataOutputStream h = new DataOutputStream(new FileOutputStream(nomFichier));
+    int x;
+    int y;
+    try{
+        x=f.readInt();
+    }
+    catch (EOFException e ){
+        recopier(g,h);
+        return;
+    }
+    try{
+        y=g.readInt();}
+        catch(EOFException e) {
+            h.writeInt(x);
+            recopier(f,h);
+            return;
+        }
+        while(true)
+        if (x<=y){
+            h.writeInt(x);
+            try{
+                x=f.readInt();}
+                catch(EOFException e){
+                    h.writeInt(y);
+                    recopier(g,h);
+                    return;
+                }
+            }
+            else{
+                h.writeInt(y);
+            try{
+                y=g.readInt();
+            }
+            catch(EOFException e){
+                h.writeInt(x);
+                recopier(f,h);
+                return;
+            }}}}
 
 
 
