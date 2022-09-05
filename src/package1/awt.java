9792da0d7367d9652879f4c62801f235081697f5
cedec1985 <cedricdecraim@msn.com>
2022-09-05 18:26:04 +0200

    awt

---
+++ src/package1/awt.java
@@ -0,0 +1,16 @@
+package package1;
+import java.awt.*;
+
+
+public class awt extends Frame {
+    static final int LARGEUR=300;
+    static final int HAUTEUR=200;
+    public awt(){
+        super("Bonjour");
+        setSize(LARGEUR,HAUTEUR);
+        setVisible(true);
+    }
+    public void paint(Graphics g){
+        g.drawString("Bonjour à tous", 110, 110);
+    }
+}
