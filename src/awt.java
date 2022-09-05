0150e397b50200e6cc243ea1cd52bbd187dc4860
cedec1985 <cedricdecraim@msn.com>
2022-09-05 20:24:50 +0200

    javax

--- src/awt.java
+++ src/awt.java
@@ -1,25 +1,44 @@
-import java.awt.Frame;
 import java.awt.Graphics;
-
+import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
-import javax.*;
+import javafx.scene.input.KeyEvent;
 import java.applet.*;
 
-public class Appletawt extends Applet implements AncestorListener {}
 
-public class awt extends Frame{
+   public class awt extends Applet implements AncestorListener {
+
     static final int LARGEUR=300;
     static final int HAUTEUR=200;
     public awt(){
-        super("Bonjour");
+        System.out.println("Bonjour");
         setSize(LARGEUR,HAUTEUR);
         setVisible(true);
-       
     }
-   public static void main(String []args){}
+    public static void main(String []args){}
         public void paint(Graphics g){
         g.drawString("Bonjour à tous", 110, 110);
-    }}
+    }
+        
+    public class awtnd extends KeyAdapter{
+    
+            public void KeyPressed(KeyEvent e){
+                if(e.getKeyChar()=='q')
+                System.exit(0);}}
+
+ 
+ @Override
+        public void ancestorAdded(AncestorEvent event) {
+          
+            
+        }
+        @Override
+        public void ancestorRemoved(AncestorEvent event) {
+            
+            
+        }
+        @Override
+        public void ancestorMoved(AncestorEvent event) {  
+        }}  
  
      
 
