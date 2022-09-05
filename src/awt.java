3c861e857952ce2abfaa4258bc9caceb01566293
cedec1985 <cedricdecraim@msn.com>
2022-09-05 20:00:59 +0200

    ok

--- src/awt.java
+++ src/awt.java
@@ -1,6 +1,6 @@
 import java.awt.Frame;
 import java.awt.Graphics;
-import java.awt.event.*;
+
 
 public class awt extends Frame{
     static final int LARGEUR=300;
@@ -10,14 +10,7 @@ public class awt extends Frame{
         setSize(LARGEUR,HAUTEUR);
         setVisible(true);
        
-    }}
-abstract class awt2 extends KeyAdapter{
-
-        public void KeyPressed(KeyEvent e){
-            if(e.getKeyChar()=='q')
-            System.exit(0);}
-
-
+    }
    public static void main(String []args){}
         public void paint(Graphics g){
         g.drawString("Bonjour à tous", 110, 110);
---
+++ src/awtnd.java
@@ -0,0 +1,10 @@
+import java.awt.event.*;
+public class awtnd 
+
+    extends KeyAdapter{
+
+        public void KeyPressed(KeyEvent e){
+            if(e.getKeyChar()=='q')
+            System.exit(0);}
+            public static void main(String []args){}
+}
