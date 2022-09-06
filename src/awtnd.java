3b7bc2b99f8a86b8d2b241abdf281ff3f786b3e9
cedec1985 <cedricdecraim@msn.com>
2022-09-06 09:57:56 +0200

    modifié

--- src/awtnd.java
+++ src/awtnd.java
@@ -1,9 +1,1 @@
-public class awtnd 
 
-    extends KeyAdapter{
-
-        public void KeyPressed(KeyEvent e){
-            if(e.getKeyChar()=='q')
-            System.exit(0);}
-            public static void main(String []args){}
-}
