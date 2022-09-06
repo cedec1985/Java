5fadc4350909fec50947c9bb050f41468bcb4ff2
cedec1985 <cedricdecraim@msn.com>
2022-09-06 10:14:32 +0200

    committed

--- src/Appletawt.java
+++ src/Appletawt.java
@@ -3,9 +3,9 @@ import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 import java.applet.*;
 
-
-   public class awt extends Applet implements AncestorListener {
-
+   
+    public class Appletawt extends Applet implements AncestorListener {
+    public class awt {
     static final int LARGEUR=300;
     static final int HAUTEUR=200;
   
@@ -13,26 +13,26 @@ import java.applet.*;
         System.out.println("Bonjour");
         setSize(LARGEUR,HAUTEUR);
         setVisible(true);}
-
+    
         public void paint(Graphics g){
-        g.drawString("Bonjour à tous", 110, 110);
-    }
-
+        g.drawString("Bonjour à tous", 110, 110);}}
+    
+    
         @Override
         public void ancestorAdded(AncestorEvent event) {
-            // TODO Auto-generated method stub
+            
             
         }
 
         @Override
         public void ancestorRemoved(AncestorEvent event) {
-            // TODO Auto-generated method stub
+            
             
         }
 
         @Override
         public void ancestorMoved(AncestorEvent event) {
-            // TODO Auto-generated method stub
+            
             
         }
         public class awtnd 
