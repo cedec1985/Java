3c6880adaa80e83f912a56fecc0f0ee1444b5ee9
cedec1985 <cedricdecraim@msn.com>
2022-09-06 21:17:25 +0200

    static void main

--- src/Appleteuroconvertisseur.java
+++ src/Appleteuroconvertisseur.java
@@ -1,26 +1,22 @@
-import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import javafx.event.ActionEvent;
-
-
 
 public class Appleteuroconvertisseur extends Applet implements ActionListener {
     TextField monnaie = new TextField("");
     Button euros = new Button("euros");
-    Button  exit = new Button("exit");
+    Button exit = new Button("exit");
     Button francs = new Button("francs");
 
+    public static void main(String[] args) {
+    }
 
     public Appleteuroconvertisseur() {
         super();
     }
 
-    @Override
     public void init() {
         setLayout(new GridLayout(2, 1));
         monnaie.setBackground(null);
-        add(monnaie);
         Panel p = new Panel();
         p.setLayout(new GridLayout(1, 2));
         p.add(euros);
@@ -30,15 +26,24 @@ public class Appleteuroconvertisseur extends Applet implements ActionListener {
         add(p);
         pack();
         setVisible(true);
-       
     }
+
+    private void add(Panel p) {
+    }
+
+    private void setLayout(GridLayout gridLayout) {
+    }
+
+    private void setVisible(boolean b) {
+    }
+
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
-    throw new UnsupportedOperationException();}
-    
+        throw new UnsupportedOperationException();
+    }
 
     private void pack() {
-        throw new UnsupportedOperationException(); 
+        throw new UnsupportedOperationException();
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -60,17 +65,23 @@ public class Appleteuroconvertisseur extends Applet implements ActionListener {
         exit.addActionListener(this);
         euros.addActionListener(this);
         francs.addActionListener(this);
-    }}
+    }
+
     class conversion {
-        private conversion(){}
-       
+        private conversion() {
+        }
+
         static final double TAUX_DE_CONVERSION = 6.55957;
-    
-        public static double convertirEnEuros (double francs){
-        return francs/TAUX_DE_CONVERSION;
+
+        public static double convertirEnEuros(double francs) {
+            return francs / TAUX_DE_CONVERSION;
         }
-        public static double convertirEnFrancs (double euros){
-        return euros*TAUX_DE_CONVERSION;
+
+        public static double convertirEnFrancs(double euros) {
+            return euros * TAUX_DE_CONVERSION;
         }
-public static void main(String[] args){ /* TODO document why this method is empty */ }}
-    
+
+        public static void main(String[] args) {
+            /* TODO document why this method is empty */ }
+    }
+}
