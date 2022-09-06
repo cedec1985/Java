09800e12418bdb1195c71370183d3f78a7887366
cedec1985 <cedricdecraim@msn.com>
2022-09-06 16:28:55 +0200

    e

--- src/Appleteuroconvertisseur.java
+++ src/Appleteuroconvertisseur.java
@@ -3,11 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import javafx.event.ActionEvent;
 
-
-
-
-    public class Appleteuroconvertisseur extends Applet implements ActionListener{
-    TextField monnaie =new TextField("");
+public class Appleteuroconvertisseur extends Applet implements ActionListener {
+    TextField monnaie = new TextField("");
     Button euros = new Button("euros"),
             exit = new Button("exit");
     Button francs = new Button("francs");
@@ -16,46 +13,54 @@ import javafx.event.ActionEvent;
     }
 
     private void add(Panel p) {
-    }        
-    public Appleteuroconvertisseur(){
-    super();}
-    public void init(){
-    setLayout(new GridLayout(2,1));
-    monnaie.setBackground(null);
-    add(monnaie);
-    Panel p = new Panel();
-    p.setLayout(new GridLayout(1,2));
-    p.add(euros);euros.addActionListener(this);
-    p.add(francs);francs.addActionListener(this);
-    add(p);
-    pack();
-    setVisible(true);
+    }
+
+    public Appleteuroconvertisseur() {
+        super();
+    }
+
+    public void init() {
+        setLayout(new GridLayout(2, 1));
+        monnaie.setBackground(null);
+        add(monnaie);
+        Panel p = new Panel();
+        p.setLayout(new GridLayout(1, 2));
+        p.add(euros);
+        euros.addActionListener(this);
+        p.add(francs);
+        francs.addActionListener(this);
+        add(p);
+        pack();
+        setVisible(true);
     }
 
     private void pack() {
     }
-    
-    
-    public static void main(String[] args){}
-   public void actionPerformed(ActionEvent e){
-    if(e.getSource()==exit) System.out.println(0);
-    double valeur=0;
-    try{
-        valeur=(Double.parseDouble(monnaie.getText()));}
-        catch (NumberFormatException ex){
+
+    public static void main(String[] args) {
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exit)
+            System.out.println(0);
+        double valeur = 0;
+        try {
+            valeur = (Double.parseDouble(monnaie.getText()));
+        } catch (NumberFormatException ex) {
             monnaie.setText("Valeur réelle erronnée");
             return;
         }
-        if(e.getSource()==euros)
-        monnaie.setText(Double.toString(conversion.convertirEnEuros(valeur)));
+        if (e.getSource() == euros)
+            monnaie.setText(Double.toString(conversion.convertirEnEuros(valeur)));
         else
-        monnaie.setText(Double.toString(conversion.convertirEnFrancs(valeur)));
+            monnaie.setText(Double.toString(conversion.convertirEnFrancs(valeur)));
 
-    exit.addActionListener((ActionListener) this);
-    euros.addActionListener((ActionListener) this);
-    francs.addActionListener((ActionListener) this);
-}
-    
+        exit.addActionListener((ActionListener) this);
+        euros.addActionListener((ActionListener) this);
+        francs.addActionListener((ActionListener) this);
+    }
+
     @Override
-    public void actionPerformed(java.awt.event.ActionEvent e) {   
-    }}
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+    }
+}
