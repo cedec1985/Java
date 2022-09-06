f831f6d1ecf68afcda63e4d6d914e1b08282c2b7
cedec1985 <cedricdecraim@msn.com>
2022-09-06 12:25:34 +0200

    applet

---
+++ src/conversion.java
@@ -0,0 +1,10 @@
+public class conversion {
+    static final double taux_de_conversion = 6.55957;
+
+    public static double convertirEnEuros (double francs){
+    return francs/taux_de_conversion;
+    }
+    public static double convertirEnFrancs (double euros){
+    return euros*taux_de_conversion;
+    }
+}
---
+++ src/euroconvertisseur.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javafx.event.ActionEvent;
+
+public class euroconvertisseur extends Frame{
+    TextField monnaie =new TextField("");
+    Button euros = new Button("euros"),
+            exit = new Button("exit");
+    Button francs = new Button("francs");
+
+private void setLayout(GridLayout gridLayout) {
+}
+
+private void add(Panel p) {
+}        
+public euroconvertisseur(){
+    super();
+    setLayout(new GridLayout(2,1));
+    monnaie.setBackground(null);
+   
+    Panel p = new Panel();
+    p.setLayout(new FlowLayout() );
+    p.add(exit);
+    p.add(euros);
+    p.add(francs); 
+    p.add(monnaie);
+    add(p);
+    pack();
+    setVisible(true);
+    
+}
+
+private void pack() {
+}
+
+private void setVisible(boolean b) {
+}
+public void actionPerformed(ActionEvent e){
+    if(e.getSource()==exit) System.out.println(0);
+    double valeur=0;
+    try{
+        valeur=(Double.parseDouble(monnaie.getText()));}
+        catch (NumberFormatException ex){
+            monnaie.setText("Valeur réelle erronnée");
+            return;
+        }
+        if(e.getSource()==euros)
+        monnaie.setText(Double.toString(conversion.convertirEnEuros(valeur)));
+        else
+        monnaie.setText(Double.toString(conversion.convertirEnFrancs(valeur)));
+
+    
+exit.addActionListener((ActionListener) this);
+euros.addActionListener((ActionListener) this);
+francs.addActionListener((ActionListener) this);
+}}
