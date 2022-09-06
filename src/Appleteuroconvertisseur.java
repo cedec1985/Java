1a8a6f110770e1eb5fc58b0c1c340cbac2a86a5a
cedec1985 <cedricdecraim@msn.com>
2022-09-06 13:31:37 +0200

    complier

--- src/Appleteuroconvertisseur.java
+++ src/Appleteuroconvertisseur.java
@@ -3,20 +3,23 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import javafx.event.ActionEvent;
 
+
+
+
     public class Appleteuroconvertisseur extends Applet implements ActionListener{
     TextField monnaie =new TextField("");
     Button euros = new Button("euros"),
             exit = new Button("exit");
     Button francs = new Button("francs");
 
-private void setLayout(GridLayout gridLayout) {
-}
+    private void setLayout(GridLayout gridLayout) {
+    }
 
-private void add(Panel p) {
-}        
-public Appleteuroconvertisseur(){
+    private void add(Panel p) {
+    }        
+    public Appleteuroconvertisseur(){
     super();}
-public void init(){
+    public void init(){
     setLayout(new GridLayout(2,1));
     monnaie.setBackground(null);
     add(monnaie);
@@ -27,12 +30,14 @@ public void init(){
     add(p);
     pack();
     setVisible(true);
-}
+    }
 
-private void pack() {
-}
-
-public void actionPerformed(ActionEvent e){
+    private void pack() {
+    }
+    
+    
+    public static void main(String[] args){}
+   public void actionPerformed(ActionEvent e){
     if(e.getSource()==exit) System.out.println(0);
     double valeur=0;
     try{
@@ -45,16 +50,12 @@ public void actionPerformed(ActionEvent e){
         monnaie.setText(Double.toString(conversion.convertirEnEuros(valeur)));
         else
         monnaie.setText(Double.toString(conversion.convertirEnFrancs(valeur)));
-    
-exit.addActionListener((ActionListener) this);
-euros.addActionListener((ActionListener) this);
-francs.addActionListener((ActionListener) this);
-}
-public static void main(String[] args) {
-}
 
-@Override
-public void actionPerformed(java.awt.event.ActionEvent e) {
+    exit.addActionListener((ActionListener) this);
+    euros.addActionListener((ActionListener) this);
+    francs.addActionListener((ActionListener) this);
+}
     
-}
-}
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {   
+    }}
