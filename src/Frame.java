e2bd3795a597b7b12e1e01841a7d1ad3454b4903
cedec1985 <cedricdecraim@msn.com>
2022-09-06 13:44:21 +0200

    frame

--- src/Frame.java
+++ src/Frame.java
@@ -1,4 +1,19 @@
 
 public class Frame {
 
+    public Frame(String string) {
+    }
+
+    public void show() {
+    }
+
+    public void setSize(int i, int j) {
+    }
+
+    public void addWindowListener(AppletApplication applet) {
+    }
+
+    public void add(String string, AppletApplication applet) {
+    }
+
 }
