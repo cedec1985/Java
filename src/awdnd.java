3f8d378d612f742d46f20a7a5e25a27b3b7519d3
cedec1985 <cedricdecraim@msn.com>
2022-09-06 10:50:17 +0200

    awtnd

---
+++ src/awdnd.java
@@ -0,0 +1,23 @@
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+import java.applet.*;
+
+public class awdnd {
+    public class Appletawt extends Applet implements AncestorListener {
+        @Override
+        public void ancestorAdded(AncestorEvent event) {
+            
+            
+        }
+
+        @Override
+        public void ancestorRemoved(AncestorEvent event) {
+            
+            
+        }
+
+        @Override
+        public void ancestorMoved(AncestorEvent event) {
+            
+        }}}
+
--- src/awt.java
+++ src/awt.java
@@ -1,26 +1,30 @@
 import java.awt.Graphics;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
-import java.applet.*;
 
-   
     
 public class awt{
     static final int LARGEUR=300;
     static final int HAUTEUR=200;
-    
+
       
     public awt(){
         System.out.println("Bonjour");
         setSize(LARGEUR,HAUTEUR);
-        setVisible(true);}
-    
+        setVisible(true);
+    }
 
-        public void paint(Graphics g){
+private void setVisible(boolean b) {
+    }
+    
+private void setSize(int largeur2, int hauteur2) {
+    }
+
+public static void main(String []args){}
+    public void paint(Graphics g){
         g.drawString("Bonjour à tous", 110, 110);}
 
         public void KeyPressed(KeyEvent e){
             if(e.getKeyChar()=='q')
-            System.exit(0);}
-       
- public static void main(String []args){}}
+            System.exit(0);
+        }
+    }
+
