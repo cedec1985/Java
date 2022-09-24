5f4d01adf783d29bc518ac0e0abe0ff70095b693
cedec1985 <cedricdecraim@msn.com>
2022-09-24 20:07:52 +0200

    validé

--- src/sapin.java
+++ src/sapin.java
@@ -1,7 +1,12 @@
 
 int [][] sapin = new sapin [3][5];
 int milieu = sapin[0].length/2;
-    for (int i=0; i<sapin.length,i++){
+
+for (int i =0;i<sapin.length;i++){
+    for (int j=-i; j<=i;j++){
+        sapin[i][milieu+j] = 1;
+    }}
+    for (int i=0; i<sapin.length;i++){
         for (int j=0;j<sapin[0].length;j++){
             if (sapin [i][j] ==0){
                 System.out.println("");
