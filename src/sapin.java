261cbccd1eb85f7b556bc74ddd0a991c4f84efe9
cedec1985 <cedricdecraim@msn.com>
2022-09-24 19:57:25 +0200

    sapin

---
+++ src/sapin.java
@@ -0,0 +1,13 @@
+
+int [][] sapin = new sapin [3][5];
+int milieu = sapin[0].length/2;
+    for (int i=0; i<sapin.length,i++){
+        for (int j=0;j<sapin[0].length;j++){
+            if (sapin [i][j] ==0){
+                System.out.println("");
+  }
+            else
+                System.out.println(".");}
+    System.out.println();
+
+    }
